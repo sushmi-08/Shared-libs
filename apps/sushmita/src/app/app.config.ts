92503b014215164c976c6841640aae130c885de5
sushmitha <sush.08.ys@gmail.com>
2025-01-03 15:51:40 +0530

    grocery app

--- apps/sushmita/src/app/app.config.ts
+++ apps/sushmita/src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { groceriesReducer} from '@shared-libs/shared-lib';
+import { GroceriesEffects } from '@shared-libs/shared-lib';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideStore({groceries: groceriesReducer}),
+    provideEffects([GroceriesEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+
   ],
 };
--- apps/sushmita/src/app/app.routes.ts
+++ apps/sushmita/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { GroceryPageComponent } from './grocery-page/grocery-page.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {path: '', component: HomeComponent},
+    {path: "grocery", component: GroceryPageComponent}
+];
